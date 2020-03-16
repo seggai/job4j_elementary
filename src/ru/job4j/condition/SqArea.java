0afe6d3d506be883564761299a5ef677b99a0e47
Admin <yuri.argunov@gmail.com>
2020-03-16 07:15:30 +0300

    9.1. String[#254769]

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(int p, int k) {
+        double rsl1 = Math.pow(p/(2.0*(k+1.0)), 2) * k;
+        return rsl1;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+        double result12 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result12);
+    }
+}
