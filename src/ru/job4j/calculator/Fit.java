e73302d9fc7614f3bf79e3a6acc24c72403e16c6
seggai <yuri.argunov@gmail.com>
2020-03-16 10:24:19 +0300

    [#254759]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(double height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+
+    public static double womanWeight(double height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double man = Fit.manWeight(180);
+        System.out.println("Man 180 is " + man);
+        double woman = womanWeight(162);
+        System.out.println("Woman 162 is "+ woman);
+    }
+}
