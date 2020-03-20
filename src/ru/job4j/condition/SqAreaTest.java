9de37e8ab29e4e85e6455bfced83557a344b2a1c
seggai <yuri.argunov@gmail.com>
2020-03-20 08:50:49 +0300

    4.1.0

--- src/ru/job4j/condition/SqAreaTest.java
+++ src/ru/job4j/condition/SqAreaTest.java
@@ -10,5 +10,6 @@ public class SqAreaTest {
         int inK = 2;
         double expected = 2;
         double out = SqArea.square(inP, inK);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
