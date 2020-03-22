f65b62d82bec4543ff3a8653ce1f94b3cca39d1e
seggai <yuri.argunov@gmail.com>
2020-03-22 16:42:03 +0300

    [#254727]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -10,4 +10,9 @@ public class MaxTest {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void  whenMax1To2Then1() {
+        int result = Max.max(2, 1);
+        assertThat(result, is(2));
+    }
 }
