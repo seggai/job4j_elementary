ad0c717f90a99d27e87fecc8d29f7c98448d8a0b
Admin <yuri.argunov@gmail.com>
2020-03-23 20:15:15 +0300

    Counter modified [#254806]

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+    @Test
+    public void whenSumEvenNumbers12() {
+        int rsl = Counter.sumByEven(-10, 12);
+        int expected = 12;
+        assertThat(rsl, is(expected));
+    }
+}
