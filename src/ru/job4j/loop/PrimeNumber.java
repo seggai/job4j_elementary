95b0a42ee5284184b199bfd0c9a4d9913fb1f928
seggai <yuri.argunov@gmail.com>
2020-03-26 21:36:14 +0300

    Count of pr. num. upd [#254800]

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -4,14 +4,8 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int i = finish; i >= 1; i--) {
-            for (int j = i - 1; j >= 1; j--) {
-                if (j == 1) {
-                    count++;
-                } else {
-                    if (i % j == 0) {
-                        break;
-                    }
-                }
+            if (CheckPrimeNumber.check(i)) {
+                count++;
             }
         }
         return count;
