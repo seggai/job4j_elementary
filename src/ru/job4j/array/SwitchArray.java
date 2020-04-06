e3896e68f1325f8c4618d6fcad2f4851252cb0e7
Admin <yuri.argunov@gmail.com>
2020-04-07 00:07:44 +0300

    Switch elements plus [#254752]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -1,6 +1,12 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+        int x = array[source];
+        array[source] = array[dest];
+        array[dest] = x;
+        return array;
+    }
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
