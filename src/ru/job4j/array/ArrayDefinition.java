84b1b44f4e14c70866c342ad9626297dfe6162e1
Admin <yuri.argunov@gmail.com>
2020-04-06 22:59:38 +0300

    Array filling  [#254747]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] pieces = new float[40];
+        String[] names = new String[4];
+        names[0] = "One";
+        names[1] = "Two";
+        names[2] = "Three";
+        names[3] = "Four";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
