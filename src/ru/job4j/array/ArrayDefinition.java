0505d89fffe196f72bacd175a3794768c274a660
Admin <yuri.argunov@gmail.com>
2020-04-06 22:55:21 +0300

    Array [#254755]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short ages = new short[10];
-        String surnames = new String[100500];
-        float pieces = new float[40];
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] pieces = new float[40];
     }
 }
