8c0cdddbc0e86871f134cdd488e39fe8f7986aab
Admin <yuri.argunov@gmail.com>
2020-04-06 23:25:59 +0300

    Array pow [#254732]

---
+++ src/ru/job4j/array/Square.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 0; i < rst.length; i++) {
+            rst[i] = i * i;
+        }
+        return rst;
+    }
+    public static void main(String[] args) {
+        int[] jack = calculate(4);
+        for (int i = 0; i < jack.length; i++) {
+            System.out.println(jack[i]);
+        }
+    }
+}
