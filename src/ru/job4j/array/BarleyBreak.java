5076af7797fb9b9d96475388e2fc2a9a56ec9981
Admin <yuri.argunov@gmail.com>
2020-04-09 02:11:47 +0300

    Two-dimensional array [#254753]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[1][1] = 1;
+        array[1][2] = 2;
+        array[1][3] = 3;
+        array[2][1] = 4;
+        array[2][2] = 5;
+        array[2][3] = 6;
+        array[3][1] = 7;
+        array[3][2] = 8;
+        array[3][3] = 9;
+    }
+}
