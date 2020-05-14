daad208bfd6f290afffc4dee0147546d13152742
Admin <yuri.argunov@gmail.com>
2020-05-14 18:30:18 +0300

    Union of two sorted arrays

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -6,11 +6,9 @@ package ru.job4j.array;
  * @version 1
  */
 
-import java.util.Arrays;
-
 public class Merge {
     /**
-     * Method merge
+     * Method merge - method has realizes merging two arrays by order
      * @param left - first array
      * @param right - second array
      * @return rsl - completed sorted array
@@ -23,7 +21,7 @@ public class Merge {
         int stopLeft = 0;
         int stopRight = 0;
         if ((left.length == 0) || (right.length == 0)) {
-           rsl = ifOneIsEmpty(left, right);
+           return ifOneIsEmpty(left, right);
         } else {
             while (count < rsl.length) {
                 if ((left[i] < right[j]) && (stopLeft == 0)) {
@@ -64,16 +62,4 @@ public class Merge {
         return rsl;
 }
 
-    /**
-     * Main
-     * @param args args
-     */
-    public static void main(String[] args) {
-        Merge process = new Merge();
-        int[] rsl = process.merge(
-                new int[] {1, 3, 5},
-                new int[] {2, 4}
-        );
-        System.out.println(Arrays.toString(rsl));
-    }
 }
