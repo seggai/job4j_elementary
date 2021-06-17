76af090273f7e4f8e6ded312e083b120271ef70b
seggai <yuri.argunov@gmail.com>
2021-06-17 16:13:20 +0300

    && [#333783 #168659]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Cinema {
     public static void permission(boolean allowByParent, boolean hasMoney) {
-        if (allowByParent && hasMoney == true) {
+        if (allowByParent && hasMoney) {
             System.out.println("I can go to the cinema.");
         } else {
             System.out.println("I can't.");
