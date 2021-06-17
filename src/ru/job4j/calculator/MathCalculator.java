c808a1b76bdd4873376b2462493260baa921fb10
seggai <yuri.argunov@gmail.com>
2021-06-17 12:17:22 +0300

    import [#504773]

---
+++ src/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double sumDifAndDiv(double first, double second) {
+        return dif(first, second) + div(first, second);
+    }
+    public static double sumAllOfThem(double first, double second) {
+        return sum(first, second) + multiply(first, second) + dif(first, second) + div(first, second);
+    }
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
